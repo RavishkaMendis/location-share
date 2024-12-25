0750223b91be972071cdb2c6ddfec4326cb9b0c6
Rav Mendis <tech.ravishka@gmail.com>
2024-12-25 16:58:06 +1100

    Simplify LocationMap component

--- web-app/src/components/LocationMap.js
+++ web-app/src/components/LocationMap.js
@@ -1,41 +1,54 @@
-useEffect(() => {
-    console.log('Checking browser environment...');
-    console.log('UserAgent:', navigator.userAgent);
-    console.log('Geolocation available:', !!navigator.geolocation);
-    console.log('WebSocket available:', !!window.WebSocket);
-  }, []);
+import React from 'react';
+import { MapContainer, TileLayer, Circle, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const LocationMap = ({ myLocation, partnerLocation }) => {
-    const [map, setMap] = useState(null);
-  
-    useEffect(() => {
-      if (map) {
-        map.invalidateSize();
-      }
-    }, [map]);
-  
-    const calculateMapCenter = () => {
-      if (!myLocation?.latitude || !partnerLocation?.latitude) {
-        return [0, 0];
-      }
-      return [
-        (myLocation.latitude + partnerLocation.latitude) / 2,
-        (myLocation.longitude + partnerLocation.longitude) / 2
-      ];
-    };
-  
-    return (
-      <div className="map-container">
-        <MapContainer
-          center={calculateMapCenter()}
-          zoom={12}
-          className="map"
-          whenCreated={setMap}
-          attributionControl={false}
-          zoomControl={false}
-        >
-          {/* ... rest of your map code ... */}
-        </MapContainer>
-      </div>
-    );
-  };
+  const defaultCenter = [0, 0];
+  const defaultZoom = 12;
+
+  return (
+    <div className="map-container">
+      <MapContainer
+        center={defaultCenter}
+        zoom={defaultZoom}
+        className="map"
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          className="dark-map"
+        />
+        
+        {myLocation && (
+          <Circle
+            center={[myLocation.latitude, myLocation.longitude]}
+            radius={100}
+            pathOptions={{ 
+              color: '#00ff9d',
+              fillColor: '#00ff9d',
+              fillOpacity: 0.7 
+            }}
+          >
+            <Popup className="custom-popup">You</Popup>
+          </Circle>
+        )}
+
+        {partnerLocation && (
+          <Circle
+            center={[partnerLocation.latitude, partnerLocation.longitude]}
+            radius={100}
+            pathOptions={{ 
+              color: '#ff00ff',
+              fillColor: '#ff00ff',
+              fillOpacity: 0.7 
+            }}
+          >
+            <Popup className="custom-popup">Partner</Popup>
+          </Circle>
+        )}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LocationMap;
